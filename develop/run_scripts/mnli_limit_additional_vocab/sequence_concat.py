3049f154c595229b7f9771f5b0bfcaaa2f2e55e0
MaFukun <thss15_mafk@163.com>
2021-11-09 10:50:54 +0800

    Change develop dir

--- develop/run_scripts/mnli_limit_additional_vocab/sequence_concat.py
+++ develop/run_scripts/mnli_limit_additional_vocab/sequence_concat.py
@@ -9,37 +9,41 @@ import jiant.shared.caching as caching
 import jiant.utils.python.io as py_io
 import jiant.utils.display as display
 import os
+import uuid
 
-EXP_DIR = "/data1/mafukun/export"
+EXP_DIR = "/data1/mafukun/export/resource"
 DEV_DIR = "/home/mafukun/GLUE/jiant/develop"
+addition = "limit_vocab"
 task_names = ["mnli_linearized_amr"]
 hf_pretrained_model_name = "roberta-base"
-run_name = "mnli_linearized_amr_all_addition_vocab2" # ["mnli_linearized_amr", "mnli_linearized_amr_limit_addition_vocab"]
+run_name = f"mnli_linearized_amr_{addition}"
+run_id = uuid.uuid4().hex
 
 # Prepare for task: download data, export model, tokenize and cache
 # downloader.download_data(task_names, f"{EXP_DIR}/tasks")
 
-# export_model.export_model(
-#     hf_pretrained_model_name_or_path=hf_pretrained_model_name,
-#     output_base_path=f"{EXP_DIR}/models/{hf_pretrained_model_name}-amr-all-addition",
-#     additional_token_path=f"{DEV_DIR}/additions.txt"
-# )
+export_model.export_model(
+    hf_pretrained_model_name_or_path=hf_pretrained_model_name,
+    output_base_path=f"{EXP_DIR}/models/{hf_pretrained_model_name}",
+    additional_token_path=f"{DEV_DIR}/additions-limit-2.txt"
+)
 
-# for task_name in task_names:
-#     tokenize_and_cache.main(tokenize_and_cache.RunConfiguration(
-#         task_config_path=f"{EXP_DIR}/tasks/configs/{task_name}_config.json",
-#         hf_pretrained_model_name_or_path=f"{EXP_DIR}/models/{hf_pretrained_model_name}-amr-all-addition/tokenizer",
-#         output_dir=f"{EXP_DIR}/cache/{task_name}",
-#         phases=["train", "val"],
-#         max_seq_length=512,
-#     ))
-#     row = caching.ChunkedFilesDataCache(f"{EXP_DIR}/cache/{task_name}/train").load_chunk(0)[0]["data_row"]
-#     print(row.input_ids)
-#     print(row.tokens)
+for task_name in task_names:
+    tokenize_and_cache.main(tokenize_and_cache.RunConfiguration(
+        task_config_path=f"{EXP_DIR}/tasks/configs/{task_name}_{addition}_config.json",
+        hf_pretrained_model_name_or_path=f"{EXP_DIR}/models/{hf_pretrained_model_name}/tokenizer",
+        output_dir=f"{EXP_DIR}/cache/{task_name}_{addition}",
+        phases=["train", "val"],
+        max_seq_length=512,
+    ))
+    row = caching.ChunkedFilesDataCache(f"{EXP_DIR}/cache/{task_name}_{addition}/train").load_chunk(0)[0]["data_row"]
+    print(row.input_ids)
+    print(row.tokens)
 
 jiant_run_config = configurator.SimpleAPIMultiTaskConfigurator(
-    task_config_base_path=f"{EXP_DIR}/tasks/configs",
-    task_cache_base_path=f"{EXP_DIR}/cache",
+    task_config_path_dict={task_name: f"{EXP_DIR}/tasks/configs/{task_name}_{addition}_config.json"
+                           for task_name in task_names},
+    task_cache_path_dict={task_name: f"{EXP_DIR}/cache/{task_name}_{addition}" for task_name in task_names},
     train_task_name_list=task_names,
     val_task_name_list=task_names,
     train_batch_size=16,
@@ -48,12 +52,12 @@ jiant_run_config = configurator.SimpleAPIMultiTaskConfigurator(
     num_gpus=1,
 ).create_config()
 os.makedirs(f"{EXP_DIR}/run_configs/", exist_ok=True)
-py_io.write_json(jiant_run_config, f"{EXP_DIR}/run_configs/{run_name}_run_config.json")
+py_io.write_json(jiant_run_config, f"{EXP_DIR}/run_configs/{run_name}/{run_id}_run_config.json")
 display.show_json(jiant_run_config)
 
 run_args = main_runscript.RunConfiguration(
-    jiant_task_container_config_path=f"{EXP_DIR}/run_configs/{run_name}_run_config.json",
-    output_dir=f"{EXP_DIR}/runs/{run_name}",
+    jiant_task_container_config_path=f"{EXP_DIR}/run_configs/{run_name}/{run_id}_run_config.json",
+    output_dir=f"{EXP_DIR}/runs/{run_name}/{run_id}",
     hf_pretrained_model_name_or_path=hf_pretrained_model_name,
     model_path=f"{EXP_DIR}/models/{hf_pretrained_model_name}/model/model.p",
     model_config_path=f"{EXP_DIR}/models/{hf_pretrained_model_name}/model/config.json",
